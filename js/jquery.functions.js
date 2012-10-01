85b0422e47f0ca9c9e1321a3263df179a267c7ae
Robert Manukyan <manukyanrobert@gmail.com>
2012-10-01 00:16:21 -0700

    Getting Closer

--- js/jquery.functions.js
+++ js/jquery.functions.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
+$(window).load(function() {
+	
 
 
+    $('.slides').cycle({
+        fx:     'scrollHorz',
+        speed:  400,
+        timeout: 7000,
+        cleartypeNoBg: true,
+        pager:  '.pager',
+        pagerAnchorBuilder: function paginate(idx, el) {
+			return '<a class="bullet' + idx + '" href="#" >&bull;</a>';
+		},
 
+    });
+
+
+		
 });
+
